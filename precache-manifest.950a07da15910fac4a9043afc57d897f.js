0c534bd9eb719ace56b37a4e6577d155f0a554c4
MaxZarubin <Maksim.Zarubin@x5.ru>
2020-02-28 10:32:55 +0300

    Updates

---
+++ precache-manifest.950a07da15910fac4a9043afc57d897f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba311b4838f4bdbf9d3bcbe82251942d",
+    "url": "/timeline/index.html"
+  },
+  {
+    "revision": "1b6cd0bf3fe610e74e00",
+    "url": "/timeline/static/css/2.400ebc83.chunk.css"
+  },
+  {
+    "revision": "41d0f90b7f50afef8e9b",
+    "url": "/timeline/static/css/main.5f3f5884.chunk.css"
+  },
+  {
+    "revision": "1b6cd0bf3fe610e74e00",
+    "url": "/timeline/static/js/2.2bdbe9e5.chunk.js"
+  },
+  {
+    "revision": "4d05604127059121c8545b8fa9a7e3f9",
+    "url": "/timeline/static/js/2.2bdbe9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d0f90b7f50afef8e9b",
+    "url": "/timeline/static/js/main.7bccf612.chunk.js"
+  },
+  {
+    "revision": "e5eb107b49443b6441c9",
+    "url": "/timeline/static/js/runtime-main.5dec25ec.js"
+  }
+]);
